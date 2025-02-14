3646bc87cf8139dbc00410dfa346e955a544e246
Jansen Morais <jansenmorais86@gmail.com>
2025-02-14 00:59:06 +0000

    adção de char

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,13 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    char codiestado[4];//
+    char codigo[4];
+    char nome[30];
+    float população;
+    float area;
+    float pib;
+    int turismo;
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
